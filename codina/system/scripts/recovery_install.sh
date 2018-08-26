20758d0644e2cfa999a606db793b52aaa32f4be9
Shilin Victor <chrono.monochrome@gmail.com>
2018-08-26 04:30:16 +0300

    Update from SergeyL

--- codina/system/scripts/recovery_install.sh
+++ codina/system/scripts/recovery_install.sh
@@ -1,14 +1,9 @@
 #!sbin/sh
 
-#if test -f /ramdisk/recovery.cpio.gz ; then
-#	exit
-#fi
-#
-#if  test -f /ramdisk/recovery.cpio ; then
-#	exit
-#fi
 rm -f /ramdisk/recovery.cpio.gz
 
 rm -f /ramdisk/recovery.cpio
 
-cp /tmp/install/bin/recovery.cpio /ramdisk/recovery.cpio
+cp /tmp/install/bin/recovery.cpio.gz /ramdisk/recovery.cpio.gz
+
+#cp /tmp/install/bin/recovery.cpio /ramdisk/recovery.cpio
---
+++ codina/system/scripts1/recovery_install.sh
@@ -0,0 +1,13 @@
+#!sbin/sh
+
+rm -f /ramdisk/recovery.cpio.gz
+
+rm -f /ramdisk/recovery.cpio
+
+rm -f /ramdisk/modules_list.txt
+
+cp /tmp/install/bin/modules_list.txt /ramdisk/modules_list.txt
+
+cp /tmp/install/bin/recovery.cpio.gz /ramdisk/recovery.cpio.gz
+
+#cp /tmp/install/bin/recovery.cpio /ramdisk/recovery.cpio
