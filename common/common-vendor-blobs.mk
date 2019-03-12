564e03331297f996016f4bbcc1d67abde0303c4c
Shilin Victor <chrono.monochrome@gmail.com>
2019-03-12 14:23:48 +0300

    u8500-common: add ALSA configs
    they haven't been included from hw/u8500
    for some reason, let's just re-introduce these configs there...
    
    Change-Id: If83e8d05e9a4ce433f4c816404870deaddd726ae

--- common/common-vendor-blobs.mk
+++ common/common-vendor-blobs.mk
@@ -221,6 +221,29 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/lib/ste_omxcomponents/libst_omxredeye_detector.so:system/lib/ste_omxcomponents/libst_omxredeye_detector.so \
     $(LOCAL_PATH)/system/lib/ste_omxcomponents/libst_omxsplitter.so:system/lib/ste_omxcomponents/libst_omxsplitter.so
 
+# ALSA
+TARGET_ALSA_CONF_DIR := system/usr/share/alsa
+LOCAL_ALSA_CONF_DIR  := $(LOCAL_PATH)/$(TARGET_ALSA_CONF_DIR)
+
+PRODUCT_COPY_FILES += \
+        $(LOCAL_ALSA_CONF_DIR)/alsa.conf:$(TARGET_ALSA_CONF_DIR)/alsa.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/dsnoop.conf:$(TARGET_ALSA_CONF_DIR)/pcm/dsnoop.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/modem.conf:$(TARGET_ALSA_CONF_DIR)/pcm/modem.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/dpl.conf:$(TARGET_ALSA_CONF_DIR)/pcm/dpl.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/default.conf:$(TARGET_ALSA_CONF_DIR)/pcm/default.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/surround51.conf:$(TARGET_ALSA_CONF_DIR)/pcm/surround51.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/surround41.conf:$(TARGET_ALSA_CONF_DIR)/pcm/surround41.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/surround50.conf:$(TARGET_ALSA_CONF_DIR)/pcm/surround50.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/dmix.conf:$(TARGET_ALSA_CONF_DIR)/pcm/dmix.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/center_lfe.conf:$(TARGET_ALSA_CONF_DIR)/pcm/center_lfe.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/surround40.conf:$(TARGET_ALSA_CONF_DIR)/pcm/surround40.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/side.conf:$(TARGET_ALSA_CONF_DIR)/pcm/side.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/iec958.conf:$(TARGET_ALSA_CONF_DIR)/pcm/iec958.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/rear.conf:$(TARGET_ALSA_CONF_DIR)/pcm/rear.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/surround71.conf:$(TARGET_ALSA_CONF_DIR)/pcm/surround71.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/front.conf:$(TARGET_ALSA_CONF_DIR)/pcm/front.conf \
+        $(LOCAL_ALSA_CONF_DIR)/cards/aliases.conf:$(TARGET_ALSA_CONF_DIR)/cards/aliases.conf
+
 # NMF Confs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/usr/share/nmf/repository/mmdsp_8500_v2/_ev/common/portstate.elf4nmf:system/usr/share/nmf/repository/mmdsp_8500_v2/_ev/common/portstate.elf4nmf \
